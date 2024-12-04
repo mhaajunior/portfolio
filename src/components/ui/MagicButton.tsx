2506cf764aca04dea82d9a02d2454d67996a9c2f
Thithut <junior29958@gmail.com>
2024-12-04 15:42:00 +0700

    complete Hero and About me

---
+++ src/components/ui/MagicButton.tsx
@@ -0,0 +1,41 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+interface MagicButtonProps {
+  title: string;
+  icon: React.ReactNode;
+  position: "left" | "right";
+  handleClick?: () => void;
+  classes?: string;
+  width?: string;
+}
+
+const MagicButton = ({
+  title,
+  icon,
+  position,
+  handleClick,
+  classes,
+  width,
+}: MagicButtonProps) => {
+  return (
+    <button
+      className={cn(
+        "relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none md:mt-5 mt-2",
+        width ? width : "w-full md:w-60"
+      )}
+      onClick={handleClick}
+    >
+      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+      <span
+        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${classes}`}
+      >
+        {position === "left" && icon}
+        {title}
+        {position === "right" && icon}
+      </span>
+    </button>
+  );
+};
+
+export default MagicButton;
